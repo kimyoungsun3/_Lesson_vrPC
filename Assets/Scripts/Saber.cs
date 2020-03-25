29ad83f249711ecfeb22a7a10c299257d6ce9cb4
kimyoungsun3 <kyssmart@naver.com>
2020-03-26 02:38:25 +0900

    2222
    2222

--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class Saber : MonoBehaviour {
 	public LayerMask mask;
 	private Vector3 previousPos;
+	private Quaternion previousRot;
 
 	void Update () {
 		RaycastHit _hit;
 		if (Physics.Raycast(transform.position, transform.forward, out _hit, 1, mask))
 		{
 			Vector3 _dragDir = transform.position - previousPos;
-			if (Vector3.Angle(_dragDir, _hit.transform.up) > 130f)
+			if (Vector3.Angle(_dragDir, _hit.transform.up) > 130f
+				|| Vector3.Angle(transform.rotation * Vector3.forward - previousRot * Vector3.forward, _hit.transform.up) > 130 )
 			{
 				Destroy(_hit.collider.gameObject);
 			}
 		}
-
+		previousRot = transform.rotation;
 		previousPos = transform.position;
 	}
 }
---
+++ Assets/Scripts/ZRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZRotate : MonoBehaviour {
+	[SerializeField] float startAngle;
+	[SerializeField] float speed;
+	private void Start()
+	{
+		transform.rotation = transform.rotation * Quaternion.Euler(Vector3.forward * startAngle);
+	}
+
+	void Update () {
+		transform.Rotate(Vector3.forward * speed * Time.deltaTime);	
+	}
+}
